96b2228ccf6262d83b6eb17db520b13eb559e32f
Arman11217 <ahrashel1213@gmail.com>
2025-05-12 23:43:59 +0600

    add more code

--- codeforces/problemset/practice.cpp
+++ codeforces/problemset/practice.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    long long a, b;
-    cin >> a >> b ;
+int main()
+{
+    char x;
+    cin >> x;
 
-    if(a>=b){
-        cout<<"Yes"<<endl;
-
+    if (x <= 'z' && x >= 'a')
+    {
+        cout << "ALPHA" << endl;
+        cout << "IS SMALL" << endl;
     }
-    else{
-        cout<<"No"<<endl;
+    else if (x <= 'Z' && x >= 'A')
+    {
+        cout << "ALPHA" << endl;
+        cout << "IS CAPITAL" << endl;
     }
 
-    
+    else if (x <= '9' && x >= '0')
+    {
+        cout << "IS DIGIT" << endl;
+    }
 }
