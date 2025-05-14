0c42e2011173308bd45deca3d9da35f5c1f93f37
Arman11217 <ahrashel1213@gmail.com>
2025-05-14 23:48:42 +0600

    add more code

--- codeforces/problemset/practice.cpp
+++ codeforces/problemset/practice.cpp
@@ -1,65 +1,37 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define be(x) x.begin(), x.end()
-#define rbe(x) x.rbegin(), x.rend()
-#define pb push_back
-#define pf push_front
-#define ppf pop_front
-#define ppb pop_back
-#define ft front
-#define bk back
+#include<bits/stdc++.h>
 using namespace std;
-void yes()
-{
-  cout<<"YES"<<endl;
-}
-void no()
-{
-  cout<<"NO"<<endl;
-}
-int main()
-{
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<int>v(n);
-        vector<int>temp(n);
-        for(int i=0;i<n;i++)
-        {
-            cin>>v[i];
-            temp[i]=v[i];
-        }   
-        
-        bool flag = true;
-        for(int i=0;i<n-1;i++)
-        {
-            if(v[i]-v[i+1]== 1 )
-            {
-                swap(v[i],v[i+1]);
-            }
+int main(){
+
+    int a,b;
+    char ch;
+    cin>>a>>ch>>b;
+
+    if(a>b ){
+        if(ch=='>'){
+             cout<<"Right"<<endl;
         }
-           
-        sort(be(temp));
-        for(int i=0;i<n;i++)
-        {  
-           if(v[i] != temp[i]) 
-           {
-              flag = false;
-              break;
-           }
-           
+        else{
+             cout<<"Wrong"<<endl;
         }
-        if(flag)
-        {
-            yes();
+       
+
+    }
+
+    else if(a<b){
+        if(ch=='<'){
+             cout<<"Right"<<endl;
         }
         else
         {
-            no();
+             cout<<"Wrong"<<endl;
         }
     }
-
+     else if(a==b ){
+        if(ch=='='){
+             cout<<"Right"<<endl;
+        }
+        else{
+             cout<<"Wrong"<<endl;
+        }
+    }
 }
