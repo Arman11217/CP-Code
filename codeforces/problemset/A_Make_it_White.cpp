cbeb5290bd7b9d39f4e32a71639d8a5e5fefc4c9
Arman11217 <ahrashel1213@gmail.com>
2025-06-02 23:11:26 +0600

    add more code

---
+++ codeforces/problemset/A_Make_it_White.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        string str;
+
+        cin >> str;
+
+        int frontB = 0, backB = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (str[i] == 'B')
+            {
+
+                frontB = i;
+                break;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == 'B')
+            {
+
+                backB = i;
+                break;
+            }
+        }
+
+        cout << ((backB - frontB) + 1) << endl;
+    }
+}
