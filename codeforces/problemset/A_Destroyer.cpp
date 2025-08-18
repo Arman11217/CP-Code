1dbf8e031d05d33caa5a1bd3b5b6b96b1368d70e
Arman11217 <ahrashel1213@gmail.com>
2025-08-18 23:49:17 +0600

    add code

---
+++ codeforces/problemset/A_Destroyer.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define endl '\n'
+#define ll  long long
+#define testCase int t;cin>>t;while(t--)
+ 
+int  main() {
+    fast;
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        vector<ll> l(n);
+        for (int i = 0; i < n; i++) cin >> l[i];
+
+        map<ll,ll> freq;
+        for (int x : l) freq[x]++;
+
+        bool ok = true;
+
+        for (auto it : freq) {
+            ll x = it.first;
+            if (x == 0) continue;
+            if (freq[x] > freq[x-1]) {
+                ok = false;
+                break;
+            }
+        }
+
+        cout << (ok ? "YES\n" : "NO\n");
+    }
+    return 0;	  	 	  	 	  	
+}
